4f061f672bb74d701cf89cd5416936ecb60239b5
ihor <l.ihor.p.mogyla@gmail.com>
2016-02-11 21:57:34 +0200

    first

--- parser.php
+++ parser.php
@@ -8,44 +8,83 @@
 
 $file = file_get_contents('./busy_day.in');
 //echo $file;
-$lines = explode('\n', $file);
+$lines = explode("\n ", $file);
 
 $X = 0;
 $Y = 0;
 $droneCount = 0; //кількість дронів
 $turnsCount = 0; //кількість ходів
 $maxPayload = 0; //максимальна масса
+$productsWeight  = [];
+$warehousesCount = 0;
+$allWarehouses = [];
+$productsTypes = 0;
+$ordersCount = 0;
 
-$productsType = 0;
 
-foreach($lines as $key => $val){
-    $line = explode(' ', $val);
-    foreach ($line as $k => $v) {
-        if($key === 0 && $k === 0){
-            $X = $v;
-        }
-
-        if($key === 0 && $k === 1){
-            $Y = $v;
-        }
-
-        if($key === 0 && $k === 2){
-            $droneCount = $v;
-        }
-
-        if($key === 0 && $k === 3){
-            $turnsCount = $v;
-        }
-
-        if($key === 0 && $k === 4){
-            echo $v;
-//            $maxPayload = $v;
+$handle = fopen('./busy_day.in', "r");
+$key = 0;
+while (($line = fgets($handle)) !== false) {
+    $arr = explode(' ', $line);
+    foreach ($arr as $k => $v) {
+        switch($key){
+            case 0:
+                switch($k){
+                    case 0:
+                        $X = intval($v);
+                        break;
+                    case 1:
+                        $Y = intval($v);
+                        break;
+                    case 2:
+                        $droneCount = intval($v);
+                        break;
+                    case 3:
+                        $turnsCount = intval($v);
+                        break;
+                    case 4:
+                        $maxPayload = intval($v);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 1:
+                $productsTypes = intval($v);
+                break;
+            case 2:
+                $productsWeight = $arr;
+                break;
+            case 3:
+                $warehousesCount = intval($v);
+                break;
+            case (4+($warehousesCount * 2) > $key): //
+                if(count($arr) === 2){
+                    $warehouseName = $arr[0] . 'x' . $arr[1];
+                }elseif(count($arr) === $productsTypes){
+                    $warehouseVal = $arr;
+                }
+                $allWarehouses[$warehouseName] = $arr;
+                break;
+            case (4+($warehousesCount * 2)):
+                $ordersCount = intval($v);
+                break;
+            default:
+                break;
         }
     }
+    $key++;
 }
-//
-//echo 'X===>' . $X . "\n";
-//echo 'Y===>' . $Y . "\n";
-//echo '$droneCount===>' . $droneCount . "\n";
-//echo '$turnsCount===>' . $turnsCount . "\n";
-//echo '$maxPayload===>' . $maxPayload . "\n";
+
+
+
+//$productsWeight[index]
+echo 'X===>' . $X . "\n";
+echo 'Y===>' . $Y . "\n";
+echo '$droneCount===>' . $droneCount . "\n";
+echo '$turnsCount===>' . $turnsCount . "\n";
+echo '$maxPayload===>' . $maxPayload . "\n";
+echo '$productsTypes===>' . $productsTypes . "\n";
+echo '$warehousesCount===>' . $warehousesCount . "\n";
+echo '$ordersCount===>' . $ordersCount . "\n";
+//echo '$warehousesCount===>'; var_dump($allWarehouses); echo "\n";
